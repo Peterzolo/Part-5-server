5cc77e0a1adb714c9a201f717bf7d6152640b6c3
Peter <petersolomon704@gmail.com>
2023-04-23 15:59:49 +0100

    Initial commit

---
+++ controllers/blog.js
@@ -0,0 +1,88 @@
+const Blog = require("../models/Blog");
+const User = require("../models/User");
+
+exports.addPost = async (req, res) => {
+  const body = req.body;
+  const userId = req.user;
+
+  try {
+    const user = await User.findById(userId);
+    const newBlog = new Blog({
+      author: user.name,
+      title: body.title,
+      url: body.url,
+      likes: body.likes,
+      userId,
+    });
+
+    const savedBlog = await newBlog.save();
+
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
+    res.status(200).json({ result: savedBlog });
+  } catch (error) {
+    res.status(403).json(error);
+  }
+};
+
+exports.getBlogs = async (req, res) => {
+  try {
+    const posts = await Blog.find().populate("userId");
+    if (!posts.length) {
+      return res.status(400).json({ message: "No post was found" });
+    }
+    res.status(200).json({ result: posts });
+  } catch (error) {
+    res.status(403).json(error);
+  }
+};
+
+exports.deleteBlog = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = req.user;
+    const blog = await Blog.findById(id);
+    if (blog.userId.toString() !== user.toString()) {
+      return res.status(403).json("You cannot delete this blog");
+    }
+    const deletedBlog = await Blog.findByIdAndRemove(id);
+    if (!deletedBlog) {
+      res.status(400).json("Could not delete blog");
+    } else {
+      res.status(200).json("Blog deleted");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.likePost = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user;
+
+  try {
+    const blog = await Blog.findById({ _id: id });
+
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+
+    console.log("GOT HERE---");
+
+    if (blog.likedBy.includes(userId)) {
+      return res.status(400).json({ error: "Blog already liked" });
+    }
+    console.log("BLOG LIKES", typeof blog.likes);
+
+    blog.likes = blog.likes + 1;
+    blog.likedBy.push(userId);
+
+    await blog.save();
+
+    res.json(blog);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
---
+++ routes/blog.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  addPost,
+  getBlogs,
+  deleteBlog,
+  likePost,
+} = require("../controllers/blog");
+const { authenticateToken } = require("../utils/middleware");
+const blogRouter = express.Router();
+
+blogRouter.post("/", authenticateToken, addPost);
+blogRouter.delete("/:id", authenticateToken, deleteBlog);
+blogRouter.get("/", getBlogs);
+blogRouter.put("/:id/like", authenticateToken, likePost);
+
+module.exports = blogRouter;
